cb6936bda7063bf5be76b88e9de50131b08de1ab
Alimedhat000 <142531761+Alimedhat000@users.noreply.github.com>
2024-10-14 04:06:35 +0300

    init

--- today.py
+++ today.py
@@ -470,7 +470,7 @@ if __name__ == '__main__':
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
     age_data, age_time = perf_counter(
-        daily_readme, datetime.datetime(2002, 7, 5))
+        daily_readme, datetime.datetime(2004, 1, 12))
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(
         loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
